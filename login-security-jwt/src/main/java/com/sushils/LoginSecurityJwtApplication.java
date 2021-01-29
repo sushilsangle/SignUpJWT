632182519590a588ddad2e4fc6c312a009b17cdb
unknown <sushilsangle.ss@gmail.com>
2021-01-29 05:25:35 -0800

    First commit

---
+++ login-security-jwt/src/main/java/com/sushils/LoginSecurityJwtApplication.java
@@ -0,0 +1,35 @@
+package com.sushils;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@SpringBootApplication
+public class LoginSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSecurityJwtApplication.class, args);
+	}
+
+	public CorsFilter corsFilter(){
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedHeader("*");
+		config.addAllowedOrigin("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**",config);
+		return new CorsFilter(source);
+
+
+	}
+
+}
---
+++ login-security-jwt/src/main/java/com/sushils/models/ERole.java
@@ -0,0 +1,8 @@
+package com.sushils.models;
+
+public enum ERole {
+	ROLE_INTERNAL,
+    ROLE_EXTERNAL,
+    ROLE_SSO,
+    ROLE_ADMIN
+}
